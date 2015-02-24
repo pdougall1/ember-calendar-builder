efc6bcfc4c90d937e53f2a6565c52de7f13559a8
Patrick <pdougall1@gmail.com>
2015-02-24 00:14:54 -0500

    rip out eventsCount

--- addon/lib/calendar.js
+++ addon/lib/calendar.js
@@ -2,7 +2,6 @@ import Month from 'ember-calendar-builder/lib/month';
 
 var Calendar = function (initialMonthKey) {
   this.months = {};
-  this.eventCount = 0;
 
   this.showMe = function (monthKey) {
     var month = this.findOrCreate(monthKey);
@@ -25,7 +24,6 @@ var Calendar = function (initialMonthKey) {
     var startTime = moment(event.get('startTime'));
     var endTime = moment(event.get('endTime'));
 
-    this.incrimentEventCount();
     this.getAllDaysBetween(startTime, endTime).forEach(function (date) {
       calendar.findOrCreate(calendar.getMonthKey(date)); // first make sure the month exists
       calendar.findDay(calendar.getDateKey(date)).addEvent(event);
@@ -39,29 +37,11 @@ var Calendar = function (initialMonthKey) {
 
     this.getAllDaysBetween(startTime, endTime).forEach(function (date) {
       calendar.findDay(calendar.getDateKey(date)).removeEvent(event);
-      calendar.decrimentEventCount();
     });
   };
 
 
   // private
-
-  // this could be DRYed up
-  this.incrimentEventCount = function () {
-    var initial = this.eventCount;
-    this.eventCount = initial + 1;
-    if (this.component) {
-      this.component.set('eventCount', initial + 1);
-    }
-  };
-
-  this.decrimentEventCount = function () {
-    var initial = this.eventCount;
-    this.eventCount = initial - 1;
-    if (this.component) {
-      this.component.set('eventCount', initial - 1);
-    }
-  };
 
   this.findOrCreate = function (monthKey) {
     var month;
